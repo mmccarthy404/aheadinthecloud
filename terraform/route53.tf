4ada7cfab6196b25c204223e9e19ad3004371c80
michael <michael.jos.mccarthy@gmail.com>
2023-07-02 19:22:06 -0400

    Added route53 records for domain and subdomain

---
+++ terraform/route53.tf
@@ -0,0 +1,25 @@
+data "aws_route53_zone" "selected" {
+  name = var.local.domain
+}
+
+resource "aws_route53_record" "domain" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = var.local.domain
+  type    = "A"
+
+  alias = {
+    name    = aws_s3_bucket.domain.bucket_domain_name
+    zone_id = aws_s3_bucket.domain.hosted_zone_id
+  }
+}
+
+resource "aws_route53_record" "subdomain" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "www.${var.local.domain}"
+  type    = "A"
+
+  alias = {
+    name    = aws_s3_bucket.domain.bucket_domain_name
+    zone_id = aws_s3_bucket.domain.hosted_zone_id
+  }
+}
