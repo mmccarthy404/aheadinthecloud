63ea4df43e73bd40b989f41376bbf9faef77199c
michael <michael.jos.mccarthy@gmail.com>
2023-08-12 15:12:47 -0400

    Remove uneeded import

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -1,5 +1,4 @@
 // import NextImage, { ImageProps } from 'next/image'
-import React from 'react'
 
 // const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
 const Image = ({ ...rest }: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>) => <img {...rest} />
