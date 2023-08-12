ba337cee365c3f0832bd481e0fe36c49b8725500
michael <michael.jos.mccarthy@gmail.com>
2023-08-12 15:11:41 -0400

    Updated typing of Image component input props

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -1,7 +1,7 @@
 // import NextImage, { ImageProps } from 'next/image'
-import { ImageProps } from 'next/image'
+import React from 'react'
 
 // const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
-const Image = ({ ...rest }: ImageProps) => <img {...rest} />
+const Image = ({ ...rest }: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>) => <img {...rest} />
 
 export default Image
