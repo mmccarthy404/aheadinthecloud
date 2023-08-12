531710f065e99baaef5fdfcf446362379e9832b7
michael <michael.jos.mccarthy@gmail.com>
2023-08-12 13:09:52 -0400

    Update NextImage to img

--- app2/components/Image.tsx
+++ app2/components/Image.tsx
@@ -1,5 +1,7 @@
-import NextImage, { ImageProps } from 'next/image'
+// import NextImage, { ImageProps } from 'next/image'
+import { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+// const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ ...rest }: ImageProps) => <img {...rest} />
 
 export default Image
