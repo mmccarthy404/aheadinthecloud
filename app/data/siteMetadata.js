edd5f5b4fba9d11a0747c00f3b40d47e4d941ae5
michael <michael.jos.mccarthy@gmail.com>
2023-08-20 21:52:58 -0400

    Updated default author

--- app/data/siteMetadata.js
+++ app/data/siteMetadata.js
@@ -16,7 +16,7 @@ const siteMetadata = {
   twitter: '',
   facebook: '',
   youtube: '',
-  linkedin: 'https://www.linkedin.com/in/michael-jos-mccarthy/',
+  linkedin: 'https://www.linkedin.com/in/michael-jos-mccarthy',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
